bf1da4ccbc29432a6dd3c0fbbcb535b4aced9349
speicon1911 <salvadorpeinado111906@gmail.com>
2025-04-01 13:29:37 +0200

    aaaa

--- src/main/java/com/daw/services/TareaService.java
+++ src/main/java/com/daw/services/TareaService.java
@@ -1,21 +1,65 @@
 package com.daw.services;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.daw.persistence.entities.Tarea;
+import com.daw.persistence.entities.enums.Estado;
 import com.daw.persistence.repositories.TareaRepository;
+import com.daw.services.exceptions.TareaNotFoundException;
 
 @Service
 public class TareaService {
 
 	@Autowired
 	private TareaRepository tareaRepository;
-	
+
 	public List<Tarea> findAll() {
 		return this.tareaRepository.findAll();
 	}
 
+	public Tarea findById(int idTarea) {
+		if(!this.tareaRepository.existsById(idTarea)) {
+			throw new TareaNotFoundException("El ID de la tarea no existe");
+		}
+		
+		return this.tareaRepository.findById(idTarea).get();
+	}
+
+	public boolean existsById(int idTarea) {
+		return this.tareaRepository.existsById(idTarea);
+	}
+
+	public boolean deleteById(int idTarea) {
+		boolean result = false;
+
+		if (this.tareaRepository.existsById(idTarea)) {
+			this.tareaRepository.deleteById(idTarea);
+			result = true;
+		}
+
+		return result;
+	}
+
+	public Tarea create(Tarea tarea) {
+		tarea.setFechaCreacion(LocalDate.now());
+		tarea.setEstado(Estado.PENDIENTE);
+
+		Tarea tareaBD = this.tareaRepository.save(tarea);
+
+		return tareaBD;
+	}
+
+	public Tarea update(Tarea tarea) {
+		Tarea tareaDB = this.findById(tarea.getId());
+		/*ESTO HACE QUE NO SE ACTUALICE EN CASO DE QUE LA MODIFIQUE
+		 * ALGUIEN YA QUE LA RECOGE DE LA BASE DE DATOS*/		
+		tarea.setFechaCreacion(tareaDB.getFechaCreacion());
+		tarea.setEstado(tareaDB.getEstado());
+
+		return this.tareaRepository.save(tarea);
+	}
 }
--- src/main/java/com/daw/web/controllers/TareaController.java
+++ src/main/java/com/daw/web/controllers/TareaController.java
@@ -3,7 +3,12 @@ package com.daw.web.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +18,7 @@ import com.daw.services.TareaService;
 @RestController
 @RequestMapping("/tareas")
 public class TareaController {
-	
+
 	@Autowired
 	private TareaService tareaService;
 
@@ -22,4 +27,23 @@ public class TareaController {
 		return this.tareaService.findAll();
 	}
 
+	@GetMapping("/{idTarea}")
+	public Tarea findById(@PathVariable int idTarea) {
+		return this.tareaService.findById(idTarea);
+	}
+
+	@DeleteMapping("/{idTarea}")
+	public boolean delete(@PathVariable int idTarea) {
+		return this.tareaService.deleteById(idTarea);
+	}
+
+	@PostMapping
+	public Tarea create(@RequestBody Tarea tarea) {
+		return this.tareaService.create(tarea);
+	}
+
+	@PutMapping("/{idTarea}")
+	public Tarea update(@PathVariable int idTarea, @RequestBody Tarea tarea) {
+		return this.tareaService.update(tarea);
+	}
 }
